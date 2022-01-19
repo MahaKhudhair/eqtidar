481377a352d9e1c78033afdaaa3a41a1da67a4af
safaa azzam <89359555+safa-azzam@users.noreply.github.com>
2022-01-20 00:15:55 +0300

    not finish

---
+++ eqtidar/src/components/CreateAccount/CreateAccount.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Footer from "./Footer";
+import Header from "./Header";
+import Info from "./Info";
+
+const CreateAccount=()=>{
+    return (
+        <div>
+        <Footer/>
+        <Header/>
+        <Info/>
+        </div>
+    )
+}
+
+export default CreateAccount;
---
+++ eqtidar/src/components/LogIn/LogIn.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+import './index.css';
+
+
+    
+const LogIn=()=>{
+    return (
+        
+            <div className="login">
+                <Header/>
+                <Info/>
+                <Footer/>
+            </div>
+    )
+}
+
+export default LogIn;
