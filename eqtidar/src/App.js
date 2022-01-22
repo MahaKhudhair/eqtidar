892dd39b38a7837d25b26b51128007b299e93a7b
Maha <mahaalsarraj85@gmail.com>
2022-01-22 22:56:54 +0300

    make nav bar

--- eqtidar/src/App.js
+++ eqtidar/src/App.js
@@ -4,14 +4,16 @@ import CreateAccount from './components/CreateAccount/CreateAccount';
 import HomePage from './components/HomePage/HomePage';
 import LogIn from './components/LogIn/LogIn';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+// import { Navbar } from 'react-bootstrap';
+import TopNavbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div>
+    <TopNavbar/>
     <HomePage/>
-    <LogIn/>
+    {/* <LogIn/>
     <CreateAccount/>
-    <AboutUs/>
+    <AboutUs/> */}
     </div>
   );
 }
